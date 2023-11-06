3a644fbfa86907cc7b3b074993d41e1fdfd2d003
Lauren Puumala <lpuumala@student.ubc.ca>
2023-11-06 14:51:41 -0800

    Initial draft of README

--- R/count_obs.R
+++ R/count_obs.R
@@ -9,11 +9,11 @@
 #' @param data A data frame;  this parameter was named `data` to make it obvious that this should be the
 #' raw data frame from which the user wants to extract information.
 #'
-#' @param group_vars The grouping variable(s). One or more grouping variables may be used. The grouping
-#' variables specify which categorical variable(s) in `data` the function should count observations in.
-#' The grouping variable must be of class chr, fct, or date. This parameter was named `group_vars` to
-#' make it obvious that this variable should be used to specify the names of the variables that should
-#' be used for grouping the data when the function is called.
+#' @param group_vars Character vector containing the name(s) of the grouping variable(s). One or more
+#' grouping variables may be used. The grouping variables specify which categorical variable(s) in `data`
+#' the function should count observations in. The grouping variable must be of class chr, fct, or date.
+#' This parameter was named `group_vars` to make it obvious that this variable should be used to specify
+#' the names of the variables that should be used for grouping the data when the function is called.
 #'
 #' @return A tibble listing the number of observations of each value of a categorical variable or
 #' combination of categorical variables specified in `group_vars` from the data frame `data`.
@@ -32,8 +32,16 @@ count_obs <- function(data, group_vars, .groups = 'drop') {
 
   # Only allow grouping by variables that belong to character, date, or factor classes
   # If any grouping variable is an incorrect class, an error message will be thrown
-  stopifnot(is.character(data |> pull({{group_vars}})) | is.factor(data |> pull({{group_vars}})) |
-             is.Date(data |> pull({{group_vars}})))
+  i <- 1 # initialize indexing variable
+
+  for (i in 1:length(group_vars)) { # loop to check variable classes in group_vars
+    if (is.character(data[[group_vars[[i]]]]) == FALSE && is.factor(data[[group_vars[[i]]]]) == FALSE &&
+        is.Date(data[[group_vars[[i]]]]) == FALSE) {
+      stop('Incorrect grouping variable class. Ensure all group_vars are of class chr, fct, or date.')
+    } else {
+      i <- i + 1 # if variable is correct type, move onto next variable in group_vars
+    }
+  }
 
   # Find the number observations for each group and return the summary table
   data |>
